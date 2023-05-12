f1753befb62c11a7dfbf8606a2dea77273da047d
Juan Santana <juanka9755@hotmail.com>
2023-05-12 08:14:20 -0500

    05-HOOKS APP
    useState
    useCounter(Custom Hook) -> useState
    
    Se crean componentes
    
    CounterApp
    CounterWithCustomHook

---
+++ src/hooks/useCounter.js
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export const useCounter = (initialValue = 0) => {
+
+    const [counter, setcounter] = useState(initialValue)
+
+    const increment = (value = 1) => {
+        setcounter( counter + value );
+    }
+
+    const decrement = (value = 1) => {
+        if (counter <= 0) return
+        setcounter( counter - value );
+    }
+
+    const reset = () => {
+        setcounter( initialValue );
+    }
+
+    return{
+        counter: counter,
+        increment: increment,
+        decrement: decrement,
+        reset: reset
+    }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { HooksApp } from './HooksApp'
+// import { CounterApp } from './01-useState/CounterApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CounterWithCustomHook />
   </React.StrictMode>,
 )
